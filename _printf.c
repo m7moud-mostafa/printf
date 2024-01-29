7021b8bca9a5d3586f6db956319645240de34652
Mahmoud <mah2002moud@gmail.com>
2024-01-29 23:04:51 +0200

    creating the printf description

--- _printf.c
+++ _printf.c
@@ -1,8 +1,13 @@
 #include "main.h"
 
 /**
- * _printf - prints formatted text
+ * _printf - write output to stdout, the standard output stream.
  * @format: creates the string that will be printed and formatted
+ * Return: the number of characters printed (excluding the null byte
+ *			used to end output to strings)
  */
 
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
+{
+	
+}
