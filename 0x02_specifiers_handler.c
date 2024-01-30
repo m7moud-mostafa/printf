84da2d5480826f7fd2c3ac705636105c7a1adad0
Mahmoud <mah2002moud@gmail.com>
2024-01-30 20:58:21 +0200

    created the specifiers_handlers.c file which contain the c_handler and s_handler functions, handled when a NULL string is passed

---
+++ 0x02_specifiers_handler.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * c_handler - handles the c specifier inside (_printf) function
+ *			   and print single characters
+ *
+ * @c: character to be printed
+ * @len: the printed length counter
+ */
+void c_handler(char c, int *len)
+{
+	write(1, &c, 1);
+	(*len)++;
+}
+
+/**
+ * s_handler - handles the s specifier inside (_printf) function (prints strings)
+ *
+ * @str: string to be printed
+ * @len: the printed length counter
+ */
+void s_handler(char *str, int *len)
+{
+	if (str == NULL)
+		str = "(null)";
+	
+	write(1, str, _strlen(str));
+	(*len) += _strlen(str);
+}
