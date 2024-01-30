cc03c2370a2026c1c8eaf44466d91e2231e3bbf5
Mahmoud <mah2002moud@gmail.com>
2024-01-30 19:44:12 +0200

    handled the c and % and s spicifiers in the printf function

--- 0x00_printf.c
+++ 0x00_printf.c
@@ -9,5 +9,44 @@
 
 int _printf(const char *format, ...)
 {
-	
+	va_list args;
+	int len = 0;
+	int i;
+	char *str;
+	char c;
+
+	va_start(args, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			switch (format[i + 1])
+			{
+			case 'c':
+				c = va_arg(args, int);
+				write(1, &c, 1);
+				len++;
+				break;
+			case 's':
+				str = va_arg(args, char *);
+				write(1, str, _strlen(str));
+				len += _strlen(str);
+				break;
+			case '%':
+				c = '%';
+				write(1, &c, 1);
+				len++;
+				break;
+			}
+			i++;
+		}
+		else
+		{
+			c = format[i];
+			write(1, &c, 1);
+			len++;
+		}
+	}
+	va_end(args);
+	return (len);
 }
