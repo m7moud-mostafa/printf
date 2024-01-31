4ce4e0cd2abc2ecc7cf23c2e26b2f9002f6442af
Mahmoud <mah2002moud@gmail.com>
2024-01-31 21:59:23 +0200

    fixing the _betty style issue

--- 0x00_printf.c
+++ 0x00_printf.c
@@ -11,6 +11,7 @@ int _printf(const char *format, ...)
 {	va_list args;
 	int len = 0;
 	int i;
+
 	if (format == NULL)
 		return (-1);
 	va_start(args, format);
@@ -23,26 +24,25 @@ int _printf(const char *format, ...)
 			if (format[i] == '\0')
 				return (-1);
 			switch (format[i])
-			{
-			case 'c':
-				c_handler(va_arg(args, int), &len);
-				break;
-			case 's':
-				s_handler(va_arg(args, char *), &len);
-				break;
-			case '%':
-				c_handler('%', &len);
-				break;
-			case 'd':
-			case 'i':
-				i_handler(va_arg(args, int), &len);
-				break;
-			default:
-				c_handler('%', &len);
-				if (format[i - 1] == ' ')
-					c_handler(' ', &len);
-				c_handler(format[i], &len);
-				break;
+			{	case 'c':
+					c_handler(va_arg(args, int), &len);
+					break;
+				case 's':
+					s_handler(va_arg(args, char *), &len);
+					break;
+				case '%':
+					c_handler('%', &len);
+					break;
+				case 'd':
+				case 'i':
+					i_handler(va_arg(args, int), &len);
+					break;
+				default:
+					c_handler('%', &len);
+					if (format[i - 1] == ' ')
+						c_handler(' ', &len);
+					c_handler(format[i], &len);
+					break;
 			}
 		}
 		else
